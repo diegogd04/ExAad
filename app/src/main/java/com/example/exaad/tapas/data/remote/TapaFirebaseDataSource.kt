aa7672d891c224b433921cd7d0c1cb3c462ff27b
diegogd04 <diego2612004@gmail.com>
2024-05-29 19:15:08 +0200

    Algunos errores corregidos

--- app/src/main/java/com/example/exaad/tapas/data/remote/TapaFirebaseDataSource.kt
+++ app/src/main/java/com/example/exaad/tapas/data/remote/TapaFirebaseDataSource.kt
@@ -1,5 +1,6 @@
 package com.example.exaad.tapas.data.remote
 
+import com.example.exaad.tapas.domain.Tapa
 import com.google.firebase.Firebase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
@@ -7,29 +8,19 @@ import kotlinx.coroutines.tasks.await
 
 class TapaFirebaseDataSource {
     private var database: DatabaseReference = Firebase.database.reference
-    var firebase = Firebase.database
+    private var firebase = Firebase.database
 
-    suspend fun get() {
-        var snapshot = firebase
+    suspend fun get(): List<Tapa?> {
+        val snapshot = firebase
             .getReference("Tapas")
             .get()
             .await()
             .children
             .map {
-                it.getValue()
+                it.getValue(DownloadFirebaseModel::class.java)
+            }.map {
+                it?.toModel()
             }
+        return snapshot
     }
-
-    /*fun get(){
-        val tapaListener = object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val tapa = snapshot.getValue<Tapa>()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("@DEV", "loadTapa:onCancelled", error.toException())
-            }
-        }
-
-    }*/
 }
